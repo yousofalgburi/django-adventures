085dab82817db695e6f59ea5d5b4d282ff388c65
Yousof Algburi <yousofalgburi@gmail.com>
2024-03-28 20:54:26 -0400

    first django app

---
+++ code-genius/code_genius/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from core.views import home, register
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('register/', register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+]
---
+++ code-genius/core/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, redirect
+from .forms import UserRegistrationForm
+
+def home(request):
+    return render(request, 'home.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'registration/register.html', {'form': form})
